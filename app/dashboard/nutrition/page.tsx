3c79fba85913c7f03a9d6fd77f8d6d6c1c743e47
kukingkux <cbmamangbae@gmail.com>
2026-02-18 00:18:45 +0700

    [KAN-43] Continue implementing main screens - Sidebar component progress

---
+++ app/dashboard/nutrition/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../../components/Sidebar/Sidebar";
+
+export default function Nutrition() {
+  return (
+    <>
+      <Sidebar />
+      <div className="">
+        <main className=""></main>
+      </div>
+    </>
+  );
+}
