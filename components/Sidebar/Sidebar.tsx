f2a65b140ab5e809eacc8e95f45d827f8056bdb4
kukingkux <cbmamangbae@gmail.com>
2026-02-19 01:03:38 +0700

    [KAN-43] Completed sidebar wireframe

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,10 +1,32 @@
 export default function Sidebar() {
   return (
-    <div className="flex flex-col fixed items-center p-8 bg-(--color-bg-light-alt)">
-      <div className="flex flex-row items-center justify-start gap-(--spacing-tiny) w-full px-5 py-2 bg-(--color-bg-light)">
-        <div className="w-10 h-10 bg-(--color-bg-light-alt)"></div>
-        <div className="w-40 h-4 bg-(--color-bg-light-alt)"></div>
+    <>
+      <div className="flex bg-(--color-bg-light)">
+        <div className="flex flex-row items-center justify-start gap-(--spacing-tiny) w-full px-5 py-2 bg-(--color-bg-light)">
+          <div className="w-10 h-10 bg-(--color-bg-light-alt)"></div>
+          <div className="w-40 h-4 bg-(--color-bg-light-alt)"></div>
+        </div>
       </div>
-    </div>
+      <div className="flex flex-col fixed items-center p-4 gap-4 h-screen bg-(--color-bg-light-alt)">
+        <div className="flex flex-row items-center justify-start gap-(--spacing-tiny) w-full px-5 py-2 bg-(--color-bg-light)">
+          <div className="w-10 h-10 bg-(--color-bg-light-alt)"></div>
+          <div className="w-40 h-4 bg-(--color-bg-light-alt)"></div>
+        </div>
+        <div className="flex flex-row items-center justify-start gap-(--spacing-tiny) w-full px-5 py-2 bg-(--color-bg-light)">
+          <div className="w-10 h-10 bg-(--color-bg-light-alt)"></div>
+          <div className="w-40 h-4 bg-(--color-bg-light-alt)"></div>
+        </div>
+        <div className="flex flex-row items-center justify-start gap-(--spacing-tiny) w-full px-5 py-2 bg-(--color-bg-light)">
+          <div className="w-10 h-10 bg-(--color-bg-light-alt)"></div>
+          <div className="w-40 h-4 bg-(--color-bg-light-alt)"></div>
+        </div>
+        <div className="flex flex-row items-center justify-start gap-(--spacing-tiny) w-full px-5 py-2 bg-(--color-bg-light)">
+          <div className="w-10 h-10 bg-(--color-bg-light-alt)"></div>
+          <div className="w-40 h-4 bg-(--color-bg-light-alt)"></div>
+        </div>
+      </div>
+    </>
   );
 }
+
+export function SidebarHeader() {}
